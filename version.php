6bb47bd61274cf063d3f005ba60392903770685f
Hilton Santos <jhilton930@gmail.com>
2026-01-14 15:00:13 -0300

    chore: remove local Grunt configuration files and bump version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_skill5';
-$plugin->version   = 2026011402; // YYYYMMDDXX format.
+$plugin->version   = 2026011403; // YYYYMMDDXX format.
 $plugin->requires  = 2022112800; // Moodle 4.1 or later.
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->release   = '1.0';
